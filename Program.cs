6d1b5abeb07f14ecd3b876c4fe2cfd14cad976c7
OxyTom <oxytom8@gmail.com>
2020-11-05 00:01:59 +0100

    better logs

--- Program.cs
+++ Program.cs
@@ -16,14 +16,44 @@ namespace Assembly_Bot
         {
             _client = new DiscordSocketClient();
             _client.Log += Log;
+
             await _client.LoginAsync(TokenType.Bot, System.IO.File.ReadLines("token.txt").First());
             await _client.StartAsync();
+
+#if DEBUG
+            await _client.SetGameAsync("je suis en labo aled !", type: ActivityType.CustomStatus);
+#else
+            await _client.SetGameAsync("Protecting the Assembly Project", type: ActivityType.CustomStatus);
+#endif
+
             await Task.Delay(-1);
         }
 
         private Task Log(LogMessage msg)
         {
-            Console.WriteLine(msg.ToString());
+            switch (message.Severity)
+            {
+                case LogSeverity.Critical:
+                case LogSeverity.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+
+                case LogSeverity.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+
+                case LogSeverity.Info:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+
+                case LogSeverity.Verbose:
+                case LogSeverity.Debug:
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+            }
+            Console.WriteLine($"{DateTime.Now,-19} [{message.Severity,8}] {message.Source}: {message.Message} {message.Exception}");
+            Console.ResetColor();
+
             return Task.CompletedTask;
         }
     }
