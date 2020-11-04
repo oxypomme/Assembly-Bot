a420bc93bf6b85db42e46153b30987b272b8d5b9
OxyTom <oxytom8@gmail.com>
2020-11-05 00:38:16 +0100

    added clean and ping commands

---
+++ Modules/AdminModule.cs
@@ -0,0 +1,30 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembly_Bot
+{
+    [Group("admin")]
+    public class AdminModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("clean", RunMode = RunMode.Async)]
+        [Summary("Cleans the specified amount of messages in the channel. Default 100.")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        [RequireBotPermission(ChannelPermission.ManageMessages)]
+        public async Task CleanAsync(int count = 100)
+        {
+            var messages = await Context.Channel.GetMessagesAsync(count + 1).FlattenAsync();
+
+            await (Context.Channel as SocketTextChannel).DeleteMessagesAsync(messages);
+
+            const int delay = 5000;
+            var msg = await ReplyAsync($"Purge completed. _This message will be deleted in {delay / 1000} seconds._");
+            await Task.Delay(delay);
+            await msg.DeleteAsync();
+        }
+    }
+}
